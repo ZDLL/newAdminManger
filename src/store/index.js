50a730a6c026c6eb7ce989a2d4c64daab4e8119e
ZDLL <836417219@qq.com>
2019-09-06 13:16:32 +0800

    添加用户模块的数据层

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import LoginModule from './module/loginModule.js'
 import MenuModule from './module/menuModule.js'
+import UserModule from './module/userModule.js'
 Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
     LoginModule,
-    MenuModule
+    MenuModule,
+    UserModule
   }
 })
---
+++ src/store/module/userModule.js
@@ -0,0 +1,26 @@
+import apiUrl from '../../config/api.js'
+import {sendRequest} from '../../comm/until.js'
+import * as types from '../type.js';
+import qs from 'qs'
+
+const state = {
+}
+// getters
+const getters = {//同步
+
+}
+const actions ={
+   
+}
+const mutations = {
+   
+
+}
+
+export default {
+    namespaced: true,//解决不同模块命名冲突的问题,需要加上所属的模块名
+    state,
+    getters,
+    actions,
+    mutations
+  }
