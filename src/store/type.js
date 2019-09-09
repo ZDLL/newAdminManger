d0ad682796e2a56cbe670fb1057d404ee592677a
ZDLL <836417219@qq.com>
2019-09-09 14:02:25 +0800

    保存代码上传

--- src/store/type.js
+++ src/store/type.js
@@ -5,7 +5,7 @@ export const USER_POST_ALERTPWD="USER_POST_ALERTPWD";//用户设置密码
 
 export const MENU_GET_LIST = 'MENU_GET_LIST' ;//获取菜单
 export const GET_CODE_VALUE ='GET_CODE_VALUE';//获取码表类型
-<<<<<<< HEAD
+
 export const POST_SAVE_MENU ='POST_SAVE_MENU';//添加菜单
 export const UPDAT_POST_MENU= 'UPDAT_POST_MENU';//更新菜单
 
@@ -15,11 +15,9 @@ export const DEL_DELETE_ROLE = "DEL_DELETE_ROLE";//删除角色
 export const PUT_ENABLE_ROLE = "PUT_ENABLE_ROLE" ; //启用角色
 export const GET_USER_ROLE_LIST ='GET_USER_ROLE_LIST'; //获取用户权限列表
 export const POST_SAVE_USER_MENU ="POST_SAVE_USER_MENU";//更新用户菜单
-=======
 
 export const USER_GET_LIST = 'USER_GET_LIST';//获取用户列表
 export const USER_RESET_PWD = 'USER_RESET_PWD';//重置密码
 export const ROLES_GET_LIST = 'ROLES_GET_LIST';//查询角色
 export const USER_SAVE = 'USER_SAVE';//保存用户
 export const USER_CHANGE_STATUS = 'USER_CHANGE_STATUS';
->>>>>>> 827bb0680f5b9af82f277638c03b7c04b24055d5
